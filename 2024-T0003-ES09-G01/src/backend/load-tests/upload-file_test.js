81d666e0bd56f8cb9257c886b3100e9b649f0120
vict0rcarvalh0 <victordecarvalho342@gmail.com>
2024-03-22 09:25:40 -0300

    feat(assets): :bento: adding assets

--- 2024-T0003-ES09-G01/src/backend/load-tests/upload-file_test.js
+++ 2024-T0003-ES09-G01/src/backend/load-tests/upload-file_test.js
@@ -2,18 +2,16 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export let options = {
-  vus: 10, // número de usuários virtuais simulados
-  duration: '30s', // duração do teste
+  vus: 10, 
+  duration: '30s',
 };
 
 export default function () {
-  let res = http.get('http://localhost:3001/link-lists/uploaded-file/file_model.csv'); // Substitua pelo URL real do seu endpoint GET
+  let res = http.get('http://localhost:3001/link-lists/uploaded-file/file_model.csv'); 
 
-  // Verifica se a solicitação foi bem-sucedida
   check(res, {
     'status is 200': (r) => r.status === 200,
   });
 
-  // Aguarda por um período de tempo aleatório entre 1 e 3 segundos antes de fazer a próxima solicitação
   sleep(Math.random() * 2 + 1);
 }
